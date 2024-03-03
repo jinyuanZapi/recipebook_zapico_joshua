4d68774228de85dd232b69929d3353cad12e720d
Zapi <joshua.zapico@student.ateneo.edu>
2024-03-04 07:30:27 +0800

    Lab 2

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient-detail', args=[str(self.pk)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.name}'
+    def get_absolute_url(self):
+        return reverse('ledger:recipe-detail', args=[str(self.pk)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredientField = models.ForeignKey('Ingredient',
+        on_delete = models.CASCADE,
+        default = 1,
+        related_name = 'recipe'
+    )
+    recipeField = models.ForeignKey('Recipe',
+        on_delete = models.CASCADE,
+        default = 1,
+        related_name = 'ingredients'
+    )
+
+    def __str__(self):
+        return f'{self.recipeField.name}: {self.ingredientField.name}, {self.quantity}'
+
+    class Meta:
+        ordering = ['ingredientField']
+        unique_together = [ 
+            ['quantity', 'ingredientField'],
+        ]
--- ledger/urls.py
+++ ledger/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import index, test, recipes_list, recipe_1, recipe_2
+from .views import index, RecipeListView, RecipeDetailView 
 
 urlpatterns = [
     path('', index, name="index"),
-    path('recipes/list', recipes_list, name="recipes_lists"),
-    path('recipe/1', recipe_1, name="recipe_1"),
-    path('recipe/2', recipe_2, name="recipe_2"),
+    path('recipes/list', RecipeListView.as_view(), name="recipes_list"),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe_<int:pk>'),
 ]
 
 app_name = "ledger"
