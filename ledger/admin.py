a1a0173f1d95a71709f580aedb0bbd1c3f990af7
Zapi <joshua.zapico@student.ateneo.edu>
2024-03-04 07:30:27 +0800

    Lab 2

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient
+
+class Inline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [Inline,]
+
+class RecipeIngredientAdmin (admin.ModelAdmin):
+    model = RecipeIngredient
+    list_display = ['ingredientField', 'quantity', 'recipeField']
+    search_fields = ['ingredientField', 'quantity', 'recipeField']
+    list_filter = ['recipeField']
+
+    fieldsets = [('Details', {'fields' : ['ingredientField', 'quantity', 'recipeField']})]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
